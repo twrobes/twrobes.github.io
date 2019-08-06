afb85a748c36558c95a63ebdf5c6e4a1e7565b2b
Thomas Wrobel <twrobel115@gmail.com>
2019-08-06 02:02:59 -0500

    Added contact.html, navigation bars, and more content

--- myscript.js
+++ myscript.js
@@ -1,1 +1,12 @@
 // Thomas Wrobel
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+}
